7bc69ebdf03129c3c2f8f179689be87d4006436b
Artur Manukyan <manukyanartur.96@mail.ru>
2020-06-10 00:56:13 +0400

    second commit

--- txt_to_csv.cpp
+++ txt_to_csv.cpp
@@ -1,12 +1,7 @@
-//#include <iostream>
 #include <fstream>
-//#include <string>
-//#include <vector>
 
-//#include "Token.h"
 #include "txt_to_csv.h"
 
-//void write(const Token* const&);
 
 void txt_to_csv(const char* input, const char* out) {
 	std::string line;
@@ -16,19 +11,16 @@ void txt_to_csv(const char* input, const char* out) {
 	{
 		getline(myfile, line);
 		if (!line.empty()) {
-			Token* t;// = new Token(line);
+			Token* t;
 			std::ofstream newfile("out.csv");
-			//newfile.open();
 			unsigned int row = 0;
+			
 			do {
 				t = new Token(line,row);
 				for(int i = 0; i < t->size(); ++i)
 					newfile << t->get_word(i)<<";";
 				newfile << "\n";
-			//	write(t);
-				//write in file csv
 				++row;
-				//std::cout << line << '\n';
 			} while (getline(myfile, line));
 			newfile.close();
 			delete t;
@@ -39,11 +31,5 @@ void txt_to_csv(const char* input, const char* out) {
 	}
 
 	else std::cout << "Unable to open file";
-
-
 }
 
-void read_file(const char* input_file_name, const char* output_file_name)
-{
-	
-}
