354dbb4fd7a1e7fe21cf424c2c1994870b23fedd
Artur Manukyan <manukyanartur.96@mail.ru>
2020-06-12 00:11:10 +0400

    delete garbages in main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include "ArgumentsParser.h"
 #include "TextToCsvConverter.h"
+
 int main(int argc, char** argv)
 {	
-	std::string inputPath;
-	std::string outputPath;
-
-	
 	ArgumentsParser argsParser(argc, argv);
 	TextToCsvConverter t(argsParser.getInputPath(), argsParser.getOutputPath());
 	t.convert("input.txt", true);
